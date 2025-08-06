846bb24d2d8683f1839d666f909612a5118873d9
emregokgedik <emregokgedik2@gmail.com>
2025-08-07 01:05:26 +0300

    Add sum calculation and display for dynamic int array

--- 08_dynamic_memory/08_dynamic_memory_01.cpp
+++ 08_dynamic_memory/08_dynamic_memory_01.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 
-int main(){
+int main() {
     int diziBoyutu;
-    std::cout<<"Kaç elemanlı dizi istersin?";
-    std::cin>>diziBoyutu;
+    std::cout << "Kaç elemanlı dizi istersiniz? ";
+    std::cin >> diziBoyutu;
     
     // Girilen boyuttaki dinamik diziyi oluşturmak
     int* dizi = new int[diziBoyutu];
 
     // Dizi elemanlarını kullanıcıdan alıp diziye ekleme
-    for(int i = 0; i<diziBoyutu;i++){
-        std::cout<<i + 1<<". elemanı girin";
-        std::cin>>dizi[i];
+    for (int i = 0; i < diziBoyutu; i++) {
+        std::cout << i + 1 << ". elemanı girin: ";
+        std::cin >> dizi[i];
     }
-    // Dizi elemanlarını ekrana yazdırma
+
+    // Dizi elemanlarının toplamını hesaplama
+    int sum = 0;
     std::cout << "\nDizi elemanları: ";
     for (int i = 0; i < diziBoyutu; i++) {
         std::cout << dizi[i] << " ";
+        sum += dizi[i];
     }
-    std::cout << std::endl;
+
+    // Toplamı ekrana yazdırma
+    std::cout << "\nToplam: " << sum << std::endl;
 
     // Belleği serbest bırakma
     delete[] dizi;
+
+    return 0;
 }
