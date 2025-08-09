5e28e50166ed7ecb629942348320e43ed5b5eb95
emregokgedik <emregokgedik2@gmail.com>
2025-08-09 16:12:07 +0300

    Add example showing floating-point rounding issue and fix with setprecision

--- 10_classes/10_classes_01.cpp
+++ 10_classes/10_classes_01.cpp
@@ -18,4 +18,18 @@ class BankaHesabi{
 };
 
 int main(){
+    BankaHesabi bankaHesabi(123456789,10000.99,"Emre Gökgedik");
+    //std::cout<<bankaHesabi.getBakiye();
+    //Üstteki kodu aktif hale getir ve hata olduğunu gör
+    std::cout<<bankaHesabi.getBakiye();
+    //çıktıyı 10001 olarak veriyor çünkü:
+    //std::cout biçimi bu değeri yuvarlayıp 10001 olarak yazıyor.
+    /*
+    1. en üste #include <iomanip> eklemesini yapın (std::fixed ve std::setprecision için)
+
+    2. Aşağıdaki kod ile gerçek değeri yazdırın (Yaşadığınız bu hatayı ve çözümü ai chatbot (gpt vs.) kullanarak araştırın)
+    
+    std::cout << std::fixed << std::setprecision(2) << bankaHesabi.getBakiye();
+
+    */
 }
